c9001321fb185d4d40c6e12c7be26ba43e703549
Fernando López Martínez <fer.lopez94@outlook.com>
2018-05-08 18:59:12 -0500

    Add general process

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const getPixels = require('get-pixels');
 
+// Process:
 // Get colors for each pixel
+// Obtain 2-dimensional array, where each element contains the rgba values for a given pixel
+// Crop 2-dimensional array to obtain values for a subimage
+   // Get predominant color for a given subimage
+   // Store predominant color in rowLetterColors array
+   // Store predominant colors for each row
+// Write letters
+
 getPixels("scene.png", function (err, pixels) {
    if (err) {
       console.log("Bad image path", err);
