09362e8a31ddc515f91b91d921b388aefd31b89f
Fernando López Martínez <fer.lopez94@outlook.com>
2018-05-08 19:04:10 -0500

    Obtain 2-dimensional array, where each element contains the rgba values for a given pixel

--- index.js
+++ index.js
@@ -21,6 +21,29 @@ getPixels("scene.png", function (err, pixels) {
    image.width = pixels.shape[0];
    image.height = pixels.shape[1];
    image.channels = pixels.shape[2];
-
    console.log('image', image);
+
+   // Obtain 2-dimensional array, where each element contains the rgba values for a given pixel
+   let i, j, index = 0;
+   let rowColors = [];
+
+   for (i = 0; i < image.height; i++) {
+      rowColors = [];
+
+      for (j = 0; j < image.width; j++) {
+         rowColors.push([pixels.data[index], pixels.data[index + 1], pixels.data[index + 2], pixels.data[index + 3]]);
+         index += image.channels;
+      }
+
+      pixelColors.push(rowColors);
+   }
+
+   console.log(pixels.data[0], pixels.data[1], pixels.data[2], pixels.data[3]);
+   console.log(pixels.data[4], pixels.data[5], pixels.data[6], pixels.data[7]);
+   console.log(pixelColors[0][0], pixelColors[0][1], pixelColors[0][2], pixelColors[0][3], pixelColors[0][4]);
+   console.log(pixelColors[1][0], pixelColors[1][1], pixelColors[1][2], pixelColors[1][3], pixelColors[1][4]);
+   // console.log('pixels.data', pixels.data);
+   console.log('pixels.data.length', pixels.data.length);
+   console.log('pixelColors.length', pixelColors.length);
+   console.log('pixelColors[0].length', pixelColors[0].length);
 });
